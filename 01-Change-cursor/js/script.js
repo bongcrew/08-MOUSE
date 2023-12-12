d6dbb802a26a18ff8fe1ca773e976e3555ff776a
bongcrew <bongcrew1@naver.com>
2023-12-12 10:22:01 +0900

    기본 완성

---
+++ 01-Change-cursor/js/script.js
@@ -0,0 +1,24 @@
+$(function () {
+  // 대상을 변수에 저장
+  const $window = $(window);
+  const $cursor = $('.cursor');
+
+  // 마우스가 움직일 때
+  $window.on('mousemove', function (e) {
+    console.log(e);
+    let mouseX = e.pageX;
+    let mouseY = e.pageY;
+
+    $cursor.css({
+      left: mouseX,
+      top: mouseY,
+    });
+  });
+
+  $window.on('mousedown', function () {
+    $cursor.addClass('click');
+  });
+  $window.on('mouseup', function () {
+    $cursor.removeClass('click');
+  });
+});
